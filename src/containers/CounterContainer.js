7928d91580ae2f0dfbdfe47a439aa5e7947a8176
hyunsang-ahn <happylovetkd@naver.com>
2022-07-08 21:53:16 +0900

    counter redux setting

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Counter from '../components/Counter';
 import { connect } from 'react-redux/es/exports';
-
+import {increse, decrese } from '../modules/counter'
 
 const CounterContainer = ({ number, increse, decrese }) => {
   return <Counter
@@ -10,17 +10,54 @@ const CounterContainer = ({ number, increse, decrese }) => {
     onDecrese={decrese}
   />
 }
-const mapStateToProps = state => ({
-  number: state.counter.number
-})
-const mapDispatchToProps = dispatch => ({
-  //임시함수
-  increse: () => {
-    console.log('increse')
-  },
-  decrese: () => {
-    console.log('decrese')
-  }
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer)
+
+
+
+
+
+
+
+
+
+// 1번방식
+// const mapStateToProps = state => ({
+//   number: state.counter.number
+// })
+// const mapDispatchToProps = dispatch => ({
+//   //임시함수
+//   increse: () => {
+//     dispatch(increse())
+//   },
+//   decrese: () => {
+//     dispatch(decrese())
+//   }
+// })
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer)
+
+
+//2번 방식
+
+
+// export default connect(
+//   state => ({
+//     number : state.counter.number,
+//   }),
+//   dispatch => ({
+//     increse : () => dispatch(increse()),
+//     decrese : () => dispatch(decrese())
+
+//   })
+// )(CounterContainer)
+
+//3번방식
+export default connect(
+  state => ({
+    number : state.counter.number,
+  }),
+  {
+    increse,
+    decrese
+  }
+)(CounterContainer)
